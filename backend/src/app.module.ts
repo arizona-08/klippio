561073777a78d43f3aabb67a8e7da5a82a213477
Jonathan Assi <assijonathan2@gmail.com>
2025-09-24 17:48:06 +0200

    ajout du dossier /user et du client prisma dans le backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ // <-- 2. L'ajouter aux imports
+      isGlobal: true,      // Rend les variables d'environnement disponibles dans toute l'app
+    }),
+    PrismaModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
